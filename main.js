4edde3e43eb9b9925ef021afcf46409e9663be8b
Kunihiko Ito <kuni.110.92@gmail.com>
2017-03-05 18:03:56 +0900

    生存・過疎・過密を実装
    初期状態は wikipedia "ライフゲーム" 記載の「ブリンカー」を参考にした
    (参考: https://ja.wikipedia.org/wiki/%E3%83%A9%E3%82%A4%E3%83%95%E3%82%B2%E3%83%BC%E3%83%A0)

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ for (var i = 0; i < 5; i++) {
   }
 }
 
+b[2][1] = "■";
+b[2][2] = "■";
+b[2][3] = "■";
+
 for (var i = 0; i < 5; i++) {
   var line = "";
   for (var j = 0; j < 5; j++) {
@@ -15,6 +19,30 @@ for (var i = 0; i < 5; i++) {
 }
 
 for (var times = 0; times < 3; times++) {
+  var bb = [[],[],[],[],[]];
+  for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
+      bb[i][j] = b[i][j]
+
+      // 生存、過疎、過密を判定
+      if (b[i][j] == "■") {
+        var count = 0;
+        if (b[i-1] && b[i-1][j] == "■") count = count+1;
+        if (b[i] && b[i][j-1] == "■") count = count+1;
+        if (b[i] && b[i][j+1] == "■") count = count+1;
+        if (b[i+1] && b[i+1][j] == "■") count = count+1;
+
+        if (count == 2) {
+          bb[i][j] = "■";
+        }　else {
+          bb[i][j] = "□";
+        }
+      }
+    }
+  }
+
+  b = bb;
+
   console.log(times + "==========");
 
   for (var i = 0; i < 5; i++) {
